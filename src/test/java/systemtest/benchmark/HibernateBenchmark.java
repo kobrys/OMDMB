a6f44dba0d4d45ecde674e0a05c8b3f07923450e
Adam Kudla <adamkudla@gmail.com>
2012-10-31 16:45:33 +0100

    Configuration and PersistenceUnit refactoring

--- src/test/java/systemtest/benchmark/HibernateBenchmark.java
+++ src/test/java/systemtest/benchmark/HibernateBenchmark.java
@@ -8,16 +8,17 @@ import javax.persistence.Persistence;
 public class HibernateBenchmark {
 
     public static void main(String[] args) {
-        EntityManager entityManager = Persistence.createEntityManagerFactory("hibernate").createEntityManager();
+//        EntityManager entityManager = Persistence.createEntityManagerFactory("hibernate").createEntityManager();
+        EntityManager entityManager = Persistence.createEntityManagerFactory("objectdb").createEntityManager();
 
         Worker x = new Worker();
         x.setName("adam");
 
-//        entityManager.getTransaction().begin();
+        entityManager.getTransaction().begin();
 
-//        entityManager.persist(x);
+        entityManager.persist(x);
 
-//        entityManager.getTransaction().commit();
-//        entityManager.close();
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 }
